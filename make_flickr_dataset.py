b764b0a46765a2c49862f7f9bf6a397bc2676abe
taeksoo <jazzsaxmafia@gmail.com>
2015-11-15 23:58:39 +0900

    맨 앞에 #START# Tag 붙임

--- make_flickr_dataset.py
+++ make_flickr_dataset.py
@@ -8,7 +8,7 @@ import cPickle
 from sklearn.feature_extraction.text import CountVectorizer
 
 annotation_path = './data/results_20130124.token'
-flickr_image_path = './images/flickr30k/'
+flickr_image_path = '../show_attend_and_tell/images/flickr30k/'
 
 annotations = pd.read_table(annotation_path, sep='\t', header=None, names=['image', 'caption'])
 annotations['image_num'] = annotations['image'].map(lambda x: x.split('#')[1])
@@ -16,17 +16,19 @@ annotations['image'] = annotations['image'].map(lambda x: os.path.join(flickr_im
 
 captions = annotations['caption'].values
 
-vectorizer = CountVectorizer(max_features=9995, token_pattern='\\b\\w+\\b').fit(captions)
+vectorizer = CountVectorizer(max_features=3000 - 3, token_pattern='\\b\\w+\\b').fit(captions)
 dictionary = vectorizer.vocabulary_
-dictionary = pd.Series(dictionary) + 5
+dictionary = pd.Series(dictionary) + 3
+#dictionary = pd.Series(dictionary) + 5
 
-dictionary['<eos>'] = 0
+dictionary['#END#'] = 0
 dictionary['UNK'] = 1
-dictionary[','] = 2
-dictionary['!'] = 3
-dictionary['?'] = 4
+dictionary['#START#'] = 2
+#dictionary[','] = 2
+#dictionary['!'] = 3
+#dictionary['?'] = 4
 
-with open('.data/dictionary.pkl', 'wb') as f:
+with open('./data/dictionary.pkl', 'wb') as f:
     cPickle.dump(dictionary, f)
 with open('./data/vectorizer.pkl', 'wb') as f:
     cPickle.dump(vectorizer, f)
--- model_tensorflow.py
+++ model_tensorflow.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+import math
 import os
 import ipdb
 import tensorflow as tf
@@ -12,8 +13,8 @@ from keras.preprocessing import sequence
 from cnn_util import *
 
 class Caption_Generator():
-    def init_weight(self, dim_in, dim_out, name=None, stddev=0.1):
-        return tf.Variable(tf.truncated_normal([dim_in, dim_out], stddev=stddev), name=name)
+    def init_weight(self, dim_in, dim_out, name=None, stddev=1.0):
+        return tf.Variable(tf.truncated_normal([dim_in, dim_out], stddev=stddev/math.sqrt(float(dim_in))), name=name)
 
     def init_bias(self, dim_out, name=None):
         return tf.Variable(tf.zeros([dim_out]), name=name)
@@ -54,6 +55,7 @@ class Caption_Generator():
             sentence_emb = tf.nn.embedding_lookup(self.Wemb, sentence)
 
         sentence_emb = tf.concat(concat_dim=1, values=[image_emb, sentence_emb])
+        sentence_emb = tf.nn.dropout(sentence_emb, 0.5)
         state = tf.zeros([self.batch_size, self.lstm.state_size])
 
         loss = 0.0
@@ -69,7 +71,8 @@ class Caption_Generator():
                 output, state = self.lstm(sentence_emb[:,i,:], state) # (batch_size, dim_hidden)
 
                 logits = tf.matmul(output, self.hidden_emb_W) + self.hidden_emb_b # (batch_size, dim_embed)
-                logits = tf.nn.tanh(logits)
+                logits = tf.nn.relu(logits)
+                logits = tf.nn.dropout(logits, 0.5)
 
                 logit_words = tf.matmul(logits, self.embed_word_W) + self.embed_word_b # (batch_size, n_words)
                 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logit_words, onehot_labels)
@@ -96,7 +99,7 @@ class Caption_Generator():
                 output, state = self.lstm(last_word, state)
 
                 logits = tf.matmul(output, self.hidden_emb_W) + self.hidden_emb_b
-                logits = tf.tanh(logits)
+                logits = tf.relu(logits)
 
                 logit_words = tf.matmul(logits, self.embed_word_W) + self.embed_word_b
                 max_prob_word = tf.argmax(logit_words, 1)
@@ -107,6 +110,45 @@ class Caption_Generator():
                 generated_words.append(max_prob_word)
 
         return image, generated_words
+
+    def generate_from_image(self, k):
+        image = tf.placeholder(tf.float32, [1, self.dim_image])
+        image_emb = tf.matmul(image, self.encode_img_W) + self.encode_img_b
+
+        state = tf.zeros([1., self.lstm.state_size])
+
+        with tf.variable_scope("RNN"):
+            output, state = self.lstm(image_emb, state)
+            tf.get_variable_scope().reuse_variables()
+            output, state = self.lstm( tf.nn.embedding_lookup(self.Wemb, [2]), state ) # 이미지 넣고 그다음 2(#START#) 넣고 시작
+            logits = tf.matmul(output, self.hidden_emb_W) + self.hidden_emb_b
+            logits = tf.relu(logits)
+
+            logit_words = tf.matmul(logits, self.embed_word_W) + self.embed_word_b
+
+            softmax = tf.nn.softmax(logit_words)
+            top_k_scores, top_k_indices = tf.nn.top_k(softmax, k)
+
+        return image, state, output, top_k_indices, top_k_scores
+
+    def generate_from_word(self, k):
+        last_state = tf.placeholder(tf.float32, [1, self.dim_hidden*2])
+        current_word = tf.placeholder(tf.int32, [1])
+        with tf.device("/cpu:0"):
+            current_emb = tf.nn.embedding_lookup(self.Wemb, current_word)
+
+        with tf.variable_scope("RNN"):
+            tf.get_variable_scope().reuse_variables()
+            output, state = self.lstm(current_emb, last_state)
+            logits = tf.matmul(output, self.hidden_emb_W) + self.hidden_emb_b
+            logits = tf.relu(logits)
+
+            logit_words = tf.matmul(logits, self.embed_word_W) + self.embed_word_b
+
+            softmax = tf.nn.softmax(logit_words)
+            top_k_scores, top_k_indices = tf.nn.top_k(softmax, k)
+
+        return last_state, current_word, state, output, top_k_indices, top_k_scores
 
 def get_caption_data(annotation_path, feat_path):
      feats = np.load(feat_path)
@@ -121,7 +163,7 @@ n_words = 10000
 dim_embed = 512
 dim_hidden = 512
 dim_image = 4096
-batch_size = 100
+batch_size = 200
 
 #learning_rate = 0.001
 n_epochs = 1000
@@ -153,17 +195,18 @@ def train():
             dim_hidden=dim_hidden,
             dim_embed=dim_embed,
             batch_size=batch_size,
-            n_lstm_steps=maxlen+1,
+            n_lstm_steps=maxlen+2,
             n_words=n_words)
 
     loss, image, sentence, mask = caption_generator.build_model()
 
-    saver = tf.train.Saver(max_to_keep=0)
+    saver = tf.train.Saver(max_to_keep=50)
     tf.initialize_all_variables().run()
 
     step = 0
     for epoch in range(n_epochs):
         train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+        #train_op = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(loss)
         for start, end in zip( \
                 range(0, len(feats), batch_size),
                 range(batch_size, len(feats), batch_size)
@@ -176,8 +219,12 @@ def train():
                     cap.lower().split(' ')[:-1]), current_captions)
 
             current_caption_matrix = sequence.pad_sequences(current_caption_ind, padding='post', maxlen=maxlen+1)
+            current_caption_matrix = np.hstack( [np.full( (len(current_caption_matrix),1), 2), current_caption_matrix] )
+            # padding을 maxlen+1만큼 해주는 건 마지막에 0를 박기 위해서임.
+
             current_mask_matrix = np.zeros((current_caption_matrix.shape[0], current_caption_matrix.shape[1]))
-            nonzeros = np.array( map(lambda x: (x != 0).sum(), current_caption_matrix ))
+            nonzeros = np.array( map(lambda x: (x != 0).sum()+1, current_caption_matrix ))
+            # 여기서 +1 해주는 건 캡션 맨 앞에 #START# (2) 를 넣었기 때문임.
 
             for ind, row in enumerate(current_mask_matrix):
                 row[:nonzeros[ind]+1] = 1
@@ -195,7 +242,7 @@ def train():
         saver.save(sess, os.path.join(model_path, 'model'), global_step=epoch)
         learning_rate *= 0.95
 
-def test(test_feat='./data/test_feat.npy', model_path='./models/model-16', maxlen=30):
+def test(test_feat='./data/test_feat2.npy', model_path='./models/model-31', maxlen=30):
 
     dictionary = pd.read_pickle(dictionary_path)
     inverse_dictionary = pd.Series(dictionary.keys(), index=dictionary.values)
@@ -221,4 +268,80 @@ def test(test_feat='./data/test_feat.npy', model_path='./models/model-16', maxle
     generated_sentence = inverse_dictionary[generated_word_index]
 
     ipdb.set_trace()
+
+def test_v2(test_feat='./data/test_feat.npy', model_path='./models/model-31', maxlen=30):
+    k = 8
+
+    dictionary = pd.read_pickle(dictionary_path)
+    inverse_dictionary = pd.Series(dictionary.keys(), index=dictionary.values)
+    feat = [np.load(test_feat)]
+    sess = tf.InteractiveSession()
+    caption_generator = Caption_Generator(
+           dim_image=dim_image,
+           dim_hidden=dim_hidden,
+           dim_embed=dim_embed,
+           batch_size=batch_size,
+           n_lstm_steps=maxlen,
+           n_words=n_words)
+
+    image, state, output, top_k_words, top_k_scores = caption_generator.generate_from_image(k=k)
+    last_state, current_word, state_, output_, top_k_words_, top_k_scores_ = caption_generator.generate_from_word(k=k)
+
+    saver = tf.train.Saver()
+    saver.restore(sess, model_path)
+
+    last_state_val = sess.run(state, feed_dict={image:feat}).repeat(k, axis=0)
+    top_k_words_val = sess.run(top_k_words, feed_dict={image:feat})[0]
+
+    last_sents = top_k_words_val#.repeat(k)[:,None]
+
+    for i in range(maxlen):
+
+        all_state = []
+        all_words = []
+        all_scores = []
+        last_sents = last_sents.repeat(k, axis=0)
+        if len(last_sents.shape ) == 1:
+            last_sents = last_sents[:,None]
+
+        for k_ in range(k): # 매 번 k*k개의 sub-sentence가 생성됨. 이 중에서 k개를 골라야 함.
+            current_state_val = sess.run(state_, feed_dict={
+                last_state:[last_state_val[k_]],
+                current_word: [top_k_words_val[k_]]
+                })
+
+            current_top_k_words_val = sess.run(top_k_words_, feed_dict={
+                last_state:[last_state_val[k_]],
+                current_word: [top_k_words_val[k_]]
+                })
+
+            current_top_k_scores_val = sess.run(top_k_scores_, feed_dict={
+                last_state:[last_state_val[k_]],
+                current_word: [top_k_words_val[k_]]
+                })
+
+            all_state.append(current_state_val)
+            all_words.append(current_top_k_words_val[0])
+            all_scores.append(current_top_k_scores_val[0])
+
+        all_state = np.vstack(all_state)
+        all_words = np.hstack(all_words)
+        all_scores = np.hstack(all_scores)
+
+        current_top_k_scores_ind = np.hstack(all_scores).argsort()[::-1][:k]
+        last_top_k_subsents = last_sents[current_top_k_scores_ind]
+        current_top_k_subsents = all_words[current_top_k_scores_ind]
+
+        last_state_val = all_state[current_top_k_scores_ind / k]
+
+        last_sents = map(lambda (x,y): x.tolist() + [y], zip(last_top_k_subsents, current_top_k_subsents))
+        last_sents = np.vstack(last_sents)
+
+        ipdb.set_trace()
+
+
+
+
+
+
 
